7da79e48ddb1edd887e0105228a2ed806802deea
Claudio Monguzzi <claudio_monguzzi@hotmail.com>
2024-03-27 19:39:41 -0300

    20240327

--- server.js
+++ server.js
@@ -37,6 +37,32 @@ app.post("/contact", (req, res) => {
     });
 });
 
+// Ruta para mostrar un mensaje de bienvenida en la raíz de la API.
+app.get("/", (req, res) => {
+  res.send(
+    <a>
+      <h1>Bienvenido a la API de Claudio Monguzzi</h1>
+      <p>Esta API permite enviar correos electrónicos desde un sitio web.</p>
+      <p>
+        Para enviar un correo electrónico, utiliza la ruta "/contact" y envía
+        los datos necesarios en el cuerpo de la solicitud.
+      </p>
+      <p>Ejemplo de solicitud:</p>
+      <pre>
+        {`{
+  "from": "Su email",
+  "text": "Hola, qué tal?",
+  "subject": "Prueba de correo electrónico"
+}`}
+      </pre>
+      <p>Para ver la documentación de la API, consulta la página de </p>
+      <a href="https://github.com/claudiomgz/sendEmail-back#readme">
+      GitHub
+      </a>
+    </a>
+  );
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
